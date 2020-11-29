0227b24caf17e2b2084edcb843ea3d29cbf2182c
xheartean <info@gokhankoc.pl>
2020-11-29 03:25:15 +0100

    initialize

---
+++ README.md
@@ -0,0 +1,9 @@
+### Using Quokka JS with React
+
+1) All setup is in package.json (quokka property)
+2) Quokka uses babel.config.js
+
+### How it looks
+![](./screenshot.png)
+### Resources
+[Quokka Configuration Docs](https://quokkajs.com//configuration.html)
---
+++ babel.config.js
@@ -0,0 +1,10 @@
+module.exports = (api) => {
+  api.cache.forever();
+  return {
+    presets: ["@babel/preset-env", "@babel/preset-react"],
+    plugins: [
+      "@babel/plugin-proposal-class-properties",
+      "@babel/plugin-proposal-object-rest-spread",
+    ],
+  };
+};
