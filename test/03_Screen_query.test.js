32da95b04c19fc25c33d17aa472676ec386da336
xheartean <info@gokhankoc.pl>
2020-12-01 01:41:44 +0100

    screen query with userEvent

--- test/03_Screen_query.test.js
+++ test/03_Screen_query.test.js
@@ -1,19 +1,21 @@
 import * as React from "react";
 import { render, fireEvent, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 import "@testing-library/jest-dom/extend-expect";
 import Counter from "../src/components/counter";
 
 test("counter increments and decrements when the buttons are clicked", () => {
   const { container } = render(<Counter />);
-  // Screen queries
+  // Screen queries (Accesibility properties)
   // (`getByRole` can work for the button too)
   const incrementButton = screen.getByText("Increment");
-  const decrementButton = screen.getByText("Decrement");
+  const decrementButton = screen.getByRole("button", { name: /decrement/i });
   const message = container.firstChild.querySelector("div");
 
   expect(message).toHaveTextContent("Current count: 0");
   fireEvent.click(incrementButton);
   expect(message).toHaveTextContent("Current count: 1");
-  fireEvent.click(decrementButton);
+  // Same thing with userEvent
+  userEvent.click(decrementButton);
   expect(message).toHaveTextContent("Current count: 0");
 });
